56729b5f5486cb132dac3ad11ca3be54d24bbeeb
TassiosVlasis <vlasis.tassios7@gmail.com>
2020-10-14 09:56:23 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+
+const PORT = process.env.PORT || 5000;
+
+const router = require("./router");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+//io connection with client
+io.on("connection", (socket) => {
+  console.log("We have a new connection");
+
+  socket.on("join", ({ name, room }, callback) => {
+    console.log(name, room);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User had left");
+  });
+});
+
+//middleware
+app.use(router);
+
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
